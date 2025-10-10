647a41938592fd1c92ded773a0427d3bc6058b1f
MashedPotato <mashedpotatouu@gmail.com>
2025-10-10 15:56:04 +0100

    Added tiebreaker

--- leaderboard_combine.py
+++ leaderboard_combine.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
-# Load both leaderboard files
-rank_adjusted_df = pd.read_csv('leaderboard_rank_adjusted.csv')
-tournament_adjusted_df = pd.read_csv('leaderboard_tournament_adjusted.csv')
+rank_adjusted_df = pd.read_csv('results/leaderboard_rank_adjusted.csv')
+tournament_adjusted_df = pd.read_csv('results/leaderboard_tournament_adjusted.csv')
 
 rank_adjusted_df = rank_adjusted_df.rename(columns={'player_id': 'user_id'})
 tournament_adjusted_df = tournament_adjusted_df.rename(columns={'player_id': 'user_id'})
@@ -15,6 +14,14 @@ tournament_adjusted_df['source'] = 'tournament_adjusted'
 concat_df = pd.concat([tournament_adjusted_df, rank_adjusted_df], ignore_index=True)
 
 final_df = concat_df.drop_duplicates(subset=['user_id'], keep='first')
-final_df_sorted = final_df.sort_values('final_rank')
-final_df_sorted = final_df_sorted[['user_id', 'username', 'final_rank']]
-final_df_sorted.to_csv('leaderboard_adjusted_what.csv', index=False)
+
+def adjust_rank_for_tiebreak(df):
+    df = df.copy()
+    mask = df['source'] == 'rank_adjusted'
+    df.loc[mask, 'final_rank'] = df.loc[mask, 'final_rank'] + 0.5
+    return df
+
+final_df_tiebreak = adjust_rank_for_tiebreak(concat_df)
+final_df_sorted = final_df_tiebreak.sort_values(by=['final_rank'])
+final_df_sorted = final_df_sorted[['user_id', 'username', 'final_rank', 'source']]
+final_df_sorted.to_csv('results/leaderboard_adjusted.csv', index=False)
